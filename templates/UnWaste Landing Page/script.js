0a5ae219066c8d6e5c484bb0bc212abb9c5060ba
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-05-12 13:38:09 -0300

    Delete templates directory

--- templates/UnWaste Landing Page/script.js
+++
@@ -1,9 +0,0 @@
-//Menu Button - Hamburger Menu
-const btnMenu = document.getElementById('btn-menu');
-
-function toggleMenu(event) {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active')
-}
-
-btnMenu.addEventListener('click', toggleMenu);
