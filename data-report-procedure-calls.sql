73bcc35023fa0f9ad1cbff4588538d1d58d22710
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-05-30 22:31:35 +0000

    [FEAT]: Added a bunch of calls.

--- data-report-procedure-calls.sql
+++ data-report-procedure-calls.sql
@@ -1,1 +1,60 @@
--- Soon :)
+CALL ObterRelatorioVendas(1);
+CALL ObterRelatorioVendas(2);
+CALL ObterRelatorioVendas(3);
+CALL ObterRelatorioVendas(4);
+CALL ObterRelatorioVendas(5);
+
+CALL ObterDetalhesVenda(1);
+CALL ObterDetalhesVenda(2);
+CALL ObterDetalhesVenda(3);
+CALL ObterDetalhesVenda(4);
+CALL ObterDetalhesVenda(5);
+CALL ObterDetalhesVenda(6);
+CALL ObterDetalhesVenda(7);
+CALL ObterDetalhesVenda(8);
+CALL ObterDetalhesVenda(9);
+CALL ObterDetalhesVenda(10);
+
+CALL ObterDetalhesVenda(11);
+CALL ObterDetalhesVenda(12);
+CALL ObterDetalhesVenda(13);
+CALL ObterDetalhesVenda(14);
+CALL ObterDetalhesVenda(15);
+CALL ObterDetalhesVenda(16);
+CALL ObterDetalhesVenda(17);
+CALL ObterDetalhesVenda(18);
+CALL ObterDetalhesVenda(19);
+CALL ObterDetalhesVenda(20);
+
+CALL ObterDetalhesVenda(21);
+CALL ObterDetalhesVenda(22);
+CALL ObterDetalhesVenda(23);
+CALL ObterDetalhesVenda(24);
+CALL ObterDetalhesVenda(25);
+CALL ObterDetalhesVenda(26);
+CALL ObterDetalhesVenda(27);
+CALL ObterDetalhesVenda(28);
+CALL ObterDetalhesVenda(29);
+CALL ObterDetalhesVenda(30);
+
+CALL ObterDetalhesVenda(31);
+CALL ObterDetalhesVenda(32);
+CALL ObterDetalhesVenda(33);
+CALL ObterDetalhesVenda(34);
+CALL ObterDetalhesVenda(35);
+CALL ObterDetalhesVenda(36);
+CALL ObterDetalhesVenda(37);
+CALL ObterDetalhesVenda(38);
+CALL ObterDetalhesVenda(39);
+CALL ObterDetalhesVenda(40);
+
+CALL ObterDetalhesVenda(41);
+CALL ObterDetalhesVenda(42);
+CALL ObterDetalhesVenda(43);
+CALL ObterDetalhesVenda(44);
+CALL ObterDetalhesVenda(45);
+CALL ObterDetalhesVenda(46);
+CALL ObterDetalhesVenda(47);
+CALL ObterDetalhesVenda(48);
+CALL ObterDetalhesVenda(49);
+CALL ObterDetalhesVenda(50);
